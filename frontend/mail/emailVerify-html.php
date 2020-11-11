6054b207c2bb152f8ce346fec26d9365ae846a06
Pham Ngoc Son <phamngocson1988@gmail.com>
2020-11-12 00:03:05 +0700

    add mail template

--- frontend/mail/emailVerify-html.php
+++ frontend/mail/emailVerify-html.php
@@ -6,10 +6,34 @@ use yii\helpers\Html;
 
 $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
 ?>
-<div class="verify-email">
-    <p>Hello <?= Html::encode($user->username) ?>,</p>
-
-    <p>Follow the link below to verify your email:</p>
-
-    <p><?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
-</div>
+<table class="email-body">
+  <tbody>
+    <tr>
+      <td class="pd-3x pdb-1-5x">
+        <h2 class="email-heading">Confirm Your E-Mail Address</h2>
+      </td>
+    </tr>
+    <tr>
+      <td class="pdl-3x pdr-3x pdb-2x">
+        <p class="mgb-1x">Hi <?= Html::encode($user->username) ?>,</p>
+        <p class="mgb-1x">Welcome! <br> You are receiving this email because you have registered on our site.</p>
+        <p class="mgb-1x">Click the link below to active your Tokenwiz account.</p>
+        <p class="mgb-2-5x">This link will expire in 15 minutes and can only be used once.</p>
+        <?= Html::a('Verify Email', $verifyLink) ?>
+      </td>
+    </tr>
+    <tr>
+      <td class="pdl-3x pdr-3x">
+        <h4 class="email-heading-s2">or</h4>
+        <p class="mgb-1x">If the button above does not work, paste this link into your web browser:</p>
+        <?= Html::a(Html::encode($verifyLink), $verifyLink, ['class' => 'link-block']) ?>
+      </td>
+    </tr>
+    <tr>
+      <td class="pd-3x pdt-2x pdb-3x">
+        <p>If you did not make this request, please contact us or ignore this message.</p>
+        <p class="email-note">This is an automatically generated email please do not reply to this email. If you face any issues, please contact us at  help@icocrypto.com</p>
+      </td>
+    </tr>
+  </tbody>
+</table>
--- frontend/mail/passwordResetToken-html.php
+++ frontend/mail/passwordResetToken-html.php
@@ -6,10 +6,26 @@ use yii\helpers\Html;
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
-<div class="password-reset">
-    <p>Hello <?= Html::encode($user->username) ?>,</p>
-
-    <p>Follow the link below to reset your password:</p>
-
-    <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
-</div>
+<table class="email-body">
+  <tbody>
+    <tr>
+      <td class="text-center pd-3x pdb-1-5x">
+        <h2 class="email-heading">Reset Password</h2>
+      </td>
+    </tr>
+    <tr>
+      <td class="text-center pd-3x pt-0 pdb-2x">
+        <p class="mgb-1x">Hi <?= Html::encode($user->username) ?>,</p>
+        <p class="mgb-2-5x">Click On The link blow to reset tour password.</p>
+        <a class="email-btn">Reset Password</a>
+        <?= Html::a('Reset Password', $resetLink, ['class' => 'email-btn']) ?>
+      </td>
+    </tr>
+    <tr>
+      <td class="text-center pd-3x pdt-2x pdb-4x">
+        <p>If you did not make this request, please contact us or ignore this message.</p>
+        <p class="email-note">This is an automatically generated email please do not reply to this email. If you face any issues, please contact us at  help@icocrypto.com</p>
+      </td>
+    </tr>
+  </tbody>
+</table>
