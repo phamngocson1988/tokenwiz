b310ca01bb8ca4e55cf0b0521f02060c7274fc39
Pham Ngoc Son <phamngocson1988@gmail.com>
2020-11-12 23:14:15 +0700

    backend mail template

--- backend/mail/layouts/html.php
+++ backend/mail/layouts/html.php
@@ -1,22 +1,77 @@
 <?php
-use yii\helpers\Html;
 
-/* @var $this \yii\web\View view component instance */
-/* @var $message \yii\mail\MessageInterface the message being composed */
-/* @var $content string main view render result */
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="js">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+  <meta charset="<?= Yii::$app->charset ?>">
+  <meta name="author" content="Softnio">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Fully featured and complete ICO Dashboard template for ICO backend management.">
+  <!-- Fav Icon  -->
+  <link rel="shortcut icon" href="/images/favicon.png">
+  <?php $this->registerCsrfMetaTags() ?>
+  <!-- Site Title  -->
+  <title><?= Html::encode($this->title) ?></title>
+  <?php $this->head() ?>
 </head>
-<body>
-    <?php $this->beginBody() ?>
-    <?= $content ?>
-    <?php $this->endBody() ?>
+<body class="page-user">
+  <?php $this->beginBody() ?>
+  <div class="page-content">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-9 col-lg-10">
+          <div class="content-area card">
+          	<div class="card-innr">
+          		<table class="email-wraper">
+							  <tbody>
+							    <tr>
+							      <td class="pdt-4x pdb-4x">
+          						<?=$content;?>
+	          					<table class="email-footer">
+							          <tbody>
+							            <tr>
+							              <td class="text-center pdt-2-5x pdl-2x pdr-2x">
+							                <p class="email-copyright-text">Copyright © 2018 Tokenwiz. <br> Template Made By <a href="https://themeforest.net/user/softnio/portfolio">Softnio</a> &amp; Handcrafted by iO.</p>
+							                <ul class="email-social">
+							                  <li><a href="#"><img src="images/brand-b.png" alt="brand"></a></li>
+							                  <li><a href="#"><img src="images/brand-e.png" alt="brand"></a></li>
+							                  <li><a href="#"><img src="images/brand-d.png" alt="brand"></a></li>
+							                  <li><a href="#"><img src="images/brand-a.png" alt="brand"></a></li>
+							                  <li><a href="#"><img src="images/brand-c.png" alt="brand"></a></li>
+							                </ul>
+							              </td>
+							            </tr>
+							          </tbody>
+							        </table>
+							      </td>
+							    </tr>
+							  </tbody>
+							</table>
+          	</div>
+            <!-- .card-innr -->
+          </div>
+          <!-- .card -->
+        </div>
+        <!-- .col -->
+      </div>
+      <!-- .row -->
+    </div>
+    <!-- .container -->
+  </div>
+  <?php $this->endBody() ?>
 </body>
 </html>
 <?php $this->endPage() ?>
